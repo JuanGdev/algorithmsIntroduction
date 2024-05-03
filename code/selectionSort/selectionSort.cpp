3c3a67d97db804c318e1f7d9456c15e6965993d7
Juan Aguilera <j.aguilerahuerta@ugto.mx>
2024-05-02 22:19:11 -0600

    Adding selection sort code

---
+++ code/selectionSort/selectionSort.cpp
@@ -0,0 +1,51 @@
+
+#include <iostream>
+#include <utility>
+#include <vector>
+
+  using namespace std;
+
+  int main()
+  {
+    int vectorElements;
+    int vectorValue;
+    int key;
+
+    //  User input
+    cout << "Enter the number of elements in your array: " << endl;
+    cin >> vectorElements;
+
+  //  Vector constructor
+  vector<int> unorderedVector(vectorElements);
+  int minIndex = 0;
+  //  Assing values to vector
+  for(int i = 0; i < vectorElements; i+=1)
+  {
+    cout << "Positon " << i << " : ";
+    cin >> vectorValue;
+    unorderedVector[i] = vectorValue;
+  }
+
+
+  //  Sort values
+  for(int current = 0; current < unorderedVector.size() - 1; current+=1)
+  {
+    minIndex = current;
+    for(int i = current+1; i<=unorderedVector.size()-1; i+=1)
+    {
+      if(unorderedVector[i] < unorderedVector[minIndex])
+      {
+        minIndex = i;
+      }
+    }
+    swap(unorderedVector[current], unorderedVector[minIndex]);
+  }
+  //  Print elements
+  cout << "\n\n***SORTED ARRAY***\n";
+  for(int i = 0; i < vectorElements; i+=1)
+  {
+    cout << "Positon " << i << ", value: " << unorderedVector[i] << endl;
+  }
+
+  return 0;
+}
